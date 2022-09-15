6dc4c2e86c857c37b7c54b9ff31cf24af4649941
Samson-Mayeem <63527034+Samson-Mayeem@users.noreply.github.com>
2022-09-15 11:51:56 +0000

    Add project files.

---
+++ SimpleLogin/Data/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleLogin.Models;
+
+namespace SimpleLogin.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+    }
+}

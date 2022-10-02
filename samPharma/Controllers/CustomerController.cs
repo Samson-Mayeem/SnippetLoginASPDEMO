18987b9f9e5a7d7ec105285c891acb9812c1e870
Samson-Mayeem <63527034+Samson-Mayeem@users.noreply.github.com>
2022-10-02 09:56:13 +0000

    custom

--- samPharma/Controllers/CustomerController.cs
+++ samPharma/Controllers/CustomerController.cs
@@ -1,12 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using samPharma.Data;
+using samPharma.Models;
 
 namespace samPharma.Controllers
 {
     public class CustomerController : Controller
     {
-        public IActionResult Index()
+        private readonly samDbContext SamDbContext;
+        public CustomerController(samDbContext SamDbContext)
         {
-            return View();
+            this.SamDbContext = SamDbContext;   
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var customer = await SamDbContext.customer.ToListAsync();
+            return View(customer);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Customer customer)
+        {
+            var cust = new Customer
+            {
+                Customer_Id = customer.Customer_Id,
+                Customer_Reference = customer.Customer_Reference,
+                Customer_Name = customer.Customer_Name,
+                Customer_Address = customer.Customer_Address,
+                Customer_Contact = customer.Customer_Contact,
+                Customer_ImageUrl = customer.Customer_ImageUrl,
+            };
+            await SamDbContext.AddAsync(cust);
+            await SamDbContext.SaveChangesAsync();
+            return RedirectToAction("Index", "Customer");
+
         }
     }
 }
